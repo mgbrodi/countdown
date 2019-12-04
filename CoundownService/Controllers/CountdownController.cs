06e0dd03720d6c830febd6d1f23bffc9bf75b3cc
Maria Gabriella Brodi <mbrodi@pivotal.io>
2019-12-04 16:50:28 -0500

    test

---
+++ CoundownService/Controllers/CountdownController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CountdownService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountdownController : ControllerBase
+    {
+        // GET api/countdown
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Big Surprise!" };
+        }
+
+        // GET api/countdown/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "You have "+id.ToString()+" seconds left! Expiration set for "+DateTime.Now.AddSeconds(id).ToString();
+        }
+    }
+}
